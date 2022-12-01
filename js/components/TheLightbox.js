baa7bb2dab61216f477c8278ad3dbb0b7616a7d7
fernandezjerome <97711003+fernandezjerome@users.noreply.github.com>
2022-12-01 00:59:01 -0500

    styling lightbox

--- js/components/TheLightbox.js
+++ js/components/TheLightbox.js
@@ -6,25 +6,28 @@ export default {
     emits: ["closelightbox"],
 
     template: `
-    <section class="lightbox">
+    <section @click="closeLB" class="lightbox">
  
        <div class="content">
-            <img @click="closeLB" src="images/closeIcon.png" class="lightbox_close">
+            <div class="content_image">
+                <img :src='"images/" + item.image' v-bind:alt="item.name">
+             
+            </div>
     
             <article class="content__copy">
                 <h3>{{item.name}}</h3>
                     <p>{{item.info}}</p>
                     <h4>CLIENT</h4>
+                  
                     <p>{{item.client}}</p>
                     <h4>Date</h4>
                     <p>{{item.date}}</p>
                     <h4>CATEGORY</h4>
                     <p>{{item.category}}</p>
             </article>
-            <div>
-                <img :src='"images/" + item.image' v-bind:alt="item.name">
-            </div>
+        
         </div>
+      
     </section>
     `,
 
