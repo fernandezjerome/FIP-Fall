1df9fd5b364925c731f210e39895c7280d1951fb
fernandezjerome <97711003+fernandezjerome@users.noreply.github.com>
2023-03-25 12:48:36 -0400

    aa

--- js/main.js
+++ js/main.js
@@ -2,6 +2,24 @@ import HeroThumb from "./components/TheHeroThumbnail.js";
 import LightBox from "./components/TheLightbox.js";
 import { SendMail } from "./components/mailer.js";
 
+const container = document.querySelector(".container");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+
+left.addEventListener("mouseenter", () =>
+    container.classList.add("hover-left")
+);
+left.addEventListener("mouseleave", () =>
+    container.classList.remove("hover-left")
+);
+
+right.addEventListener("mouseenter", () =>
+    container.classList.add("hover-right")
+);
+right.addEventListener("mouseleave", () =>
+    container.classList.remove("hover-right")
+);
+
 const phpResponse = document.getElementById("php-copy");
 
 (() => {
