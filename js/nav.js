2b2961c2c7b5eb170239461a72d823de98326b5f
fernandezjerome <97711003+fernandezjerome@users.noreply.github.com>
2023-03-25 17:55:05 -0400

    aa

--- js/nav.js
+++ js/nav.js
@@ -10,3 +10,18 @@ btnHamburger.addEventListener("click", function () {
         sideNav.classList.add("nav-slide");
     }
 });
+
+const panels = document.querySelectorAll(".case__panel");
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        panel.classList.add("active");
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    });
+}
